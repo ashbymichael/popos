8eb6e9cb7e656c0919ccae6e482ff6a98e8ded8a
Michael Ashby <ashby.michael@gmail.com>
2016-04-09 15:09:56 -0700

    add popos import function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409213521) do
+ActiveRecord::Schema.define(version: 20160409220539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160409213521) do
     t.string   "hours"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|
