c9f90d79717f5d81d6dad41d18276a0dfb9759ca
Michael Ashby <ashby.michael@gmail.com>
2016-04-09 15:47:24 -0700

    add popos import

--- app/models/popo.rb
+++ app/models/popo.rb
@@ -1,16 +1,17 @@
 class Popo < ActiveRecord::Base
   def self.import(file)
-    CSV.foreach(file.tempfile, headers: false) do |row|
-      popos = Popo.new(name: row[1],
-                       lat: row[3],
-                       long: row[4],
-                       restrooms: row[17],
-                       address: row[2],
-                       image: row[7],
-                       description: row[29],
-                       hours: row[31],
-                       location: row[20])
-      student.save if student.valid?
+    CSV.foreach(file.tempfile, headers: true) do |row|
+      # puts row[0]
+      popos = Popo.new(name: row[0],
+                       lat: row[2],
+                       long: row[3],
+                       restrooms: row[7],
+                       address: row[1],
+                       image: row[5],
+                       description: row[8],
+                       hours: row[10],
+                       location: row[9])
+      popos.save if popos.valid?
     end
   end
 end
