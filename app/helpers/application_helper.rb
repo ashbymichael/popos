4e955d6bda6d9f93af3af2eb97efbd5d566775af
James Bomotti <zilliahix@gmail.com>
2016-04-09 15:45:03 -0700

    Adds sort_popos_by_distance helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,24 @@ module ApplicationHelper
     }
   end
 
-  def get_distance(origin, destination)
+  def get_distance(args = {})
+    origin = args.fetch(:origin)
+    destination = args.fetch(:destination)
     res = HTTParty.post("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{origin[:lat]},#{origin[:long]}&destinations=#{destination[:lat]},#{destination[:long]}&key=AIzaSyCl5_4Ag_J7PW8yV-ps5ir0PMlt5a8Gp4A")
     res['rows'][0]['elements'][0]['distance']['text'][0..-4].to_f
   end
 
+  def sort_popos_by_distance(args = {})
+    lat = args.fetch(:lat, grab_location[:lat])
+    long = args.fetch(:long, grab_location[:long])
+    id_and_distance_array = []
 
+
+    Popo.all.each do |popo|
+      distance = get_distance(:origin=>{lat: lat, long: long}, :destination=>{lat: popo.lat, long: popo.long})
+      id_and_distance_array << {popo.id => distance}
+    end
+    id_and_distance_array.sort_by! { |k| k.values[0] }
+  end
+  
 end
