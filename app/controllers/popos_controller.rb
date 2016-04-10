32b8ce7a76eb563c317e456e355867f735afd947
Amir Fayek <afayek88@gmail.com>
2016-04-09 21:17:19 -0700

    add map view

--- app/controllers/popos_controller.rb
+++ app/controllers/popos_controller.rb
@@ -18,5 +18,17 @@ class PoposController < ApplicationController
 
   def index
     @popos = sort_popos_by_distance
+    p "*" * 100
+    p @popos
+
+  end
+
+  def map_markers
+    @popos = Popo.all
+    respond_to do |format|
+        format.html { redirect_to @popos, notice: 'User was successfully created.' }
+        format.js   {}
+        format.json { render json: @popos }
+    end
   end
 end
