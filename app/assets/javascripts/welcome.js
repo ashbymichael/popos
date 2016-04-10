1035b4644c79734312a8bbdcca0ceb0d714fc2f0
Amir Fayek <afayek88@gmail.com>
2016-04-09 21:17:19 -0700

    add map view

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,6 +2,9 @@ $(document).on("page:change", function () {
   'use strict';
   var button = document.getElementById("get-location");
   var currentLocation = document.getElementById("current-location");
+  var lat;
+  var lng;
+  var map;
 
   var getLocation = function () {
     if (navigator.geolocation) {
@@ -11,9 +14,67 @@ $(document).on("page:change", function () {
     }
   }
 
+  var initMap = function () {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: lat, lng: lng},
+      scrollwheel: false,
+      zoom: 15
+    });
+
+    var marker = new google.maps.Marker({
+      position: {lat: lat, lng: lng},
+      map: map,
+      title: 'Current Position'
+    });
+  }
+
+  var addPopoMarkersToMap = function () {
+    var req = $.ajax({
+      url: "/markers",
+      dataType: "json"
+    });
+
+    req.done(function (data) {
+      var popos = data;
+      var infoWindow = new google.maps.InfoWindow();
+
+      for (var i = 0; i < popos.length; i++) {
+        var data = popos[i];
+
+        var myLatlng = new google.maps.LatLng(data.lat, data.long);
+
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: data.name
+        });
+
+        (function (marker, data) {
+            var source   = $("#info-window").html();
+            var template = Handlebars.compile(source);
+            var context = data;
+            var html = template(context);
+
+            google.maps.event.addListener(marker, "click", function (e) {
+                infoWindow.setContent(html);
+                infoWindow.open(map, marker);
+            });
+        })(marker, data);
+      };
+    })
+
+    req.fail(function () {
+      console.log("fail")
+    })
+  }
+
   var showPosition = function (position) {
-    currentLocation.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+    currentLocation.innerHTML = "Latitude: " + lat +
+    "<br>Longitude: " + lng;
+    initMap();
+    addPopoMarkersToMap();
   }
 
   button.addEventListener("click", getLocation);
