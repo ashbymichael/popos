f05a4594278fb9eacfa1a1f04ad452d42d320640
benvogcodes <benjamin.voglewede@gmail.com>
2016-04-10 20:35:26 -0700

    Fix application_helper api issue

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,36 @@
+module ApplicationHelper
+
+  def grab_location
+    res = HTTParty.post("https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV['GOOGLE_KEY']}")
+    return {
+      lat:  res['location']['lat'],
+      long: res['location']['lng']
+    }
+  end
+
+  def get_distance(args = {})
+    origin = args.fetch(:origin)
+    destination = args.fetch(:destination)
+    res = HTTParty.post("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{origin[:lat]},#{origin[:long]}&destinations=#{destination[:lat]},#{destination[:long]}&key=#{ENV['GOOGLE_KEY']}")
+    res['rows'][0]['elements'][0]['distance']['text'][0..-4].to_f
+  end
+
+  def sort_popos_by_distance(args = {})
+    lat = args.fetch(:lat, grab_location[:lat])
+    long = args.fetch(:long, grab_location[:long])
+    id_and_distance_array = []
+
+
+    Popo.all.each do |popo|
+      distance = get_distance(:origin=>{lat: lat, long: long}, :destination=>{lat: popo.lat, long: popo.long})
+      id_and_distance_array << {popo => distance}
+    end
+    id_and_distance_array.sort_by! { |k| k.values[0] }
+    extract_popos(id_and_distance_array)
+  end
+
+  def extract_popos(array)
+    array.map {|popo_hash| popo_hash.keys[0]}
+  end
+
+end
